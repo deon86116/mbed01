dd03250128fd9431f5cd8a7e1a54979a39fa9584
deon86116 <deon86116@gmail.com>
2020-03-11 18:43:29 +0800

    aa

--- BUILD/K66F/GCC_ARM/main.d
+++ BUILD/K66F/GCC_ARM/main.d
@@ -1,5 +1,5 @@
-BUILD/K66F/GCC_ARM/main.o: main.cpp BUILD/K66F/GCC_ARM/mbed_config.h \
- /home/ee2405/ee2405/mbed-os/mbed.h \
+../BUILD/K66F/GCC_ARM/main.o: main.cpp \
+ ../BUILD/K66F/GCC_ARM/mbed_config.h /home/ee2405/ee2405/mbed-os/mbed.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_version.h \
  /home/ee2405/ee2405/mbed-os/rtos/rtos.h \
  /home/ee2405/ee2405/mbed-os/rtos/mbed_rtos_storage.h \
